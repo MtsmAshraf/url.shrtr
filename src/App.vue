66dcd9208663e77e3e7cdfedbbe7245ae1b251b8
MtsmAshraf <95532502+MtsmAshraf@users.noreply.github.com>
2023-02-19 07:47:50 +0200

    Font awesome import statements commented

--- src/App.vue
+++ src/App.vue
@@ -59,16 +59,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+//import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faArrowLeft, faClockRotateLeft, faLink } from '@fortawesome/free-solid-svg-icons'
+//import { faArrowLeft, faClockRotateLeft, faLink } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faLink,faClockRotateLeft,faArrowLeft)
+//library.add(faLink,faClockRotateLeft,faArrowLeft)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
