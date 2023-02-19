f4dfa2f2e0694a63053ae0145b7133a1a82a9f30
MtsmAshraf <95532502+MtsmAshraf@users.noreply.github.com>
2023-02-19 07:45:29 +0200

    Font awesome tags commented

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@
         <form>
           <input type="text" v-model.lazy="inputUrl" @focus="shortenedUrl = '', loaderShown = false" placeholder="Enter a Valid URL" autofocus>
           <button @click.prevent="getShortUrl(inputUrl),loaderShown = true">Shorten</button>
-          <button @click.prevent="flipContent(), this.showResult = false" class="history"><font-awesome-icon icon="fa-solid fa-clock-rotate-left" /></button>
+          <button @click.prevent="flipContent(), this.showResult = false" class="history">
+          <!--<font-awesome-icon icon="fa-solid fa-clock-rotate-left" />-->
+          </button>
         </form>
         <div v-if="showResult" class="result">
           <span v-if="inputUrl">The URL: </span>
@@ -29,13 +31,17 @@
           <button @click.prevent="copyingMethod()">
             <p>Copy to Clipboard</p>
             <p><svg fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M562.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L405.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C189.5 251.2 196 330 246 380c56.5 56.5 148 56.5 204.5 0L562.8 267.7zM43.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C57 372 57 321 88.5 289.5L200.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C416.5 260.8 410 182 360 132c-56.5-56.5-148-56.5-204.5 0L43.2 244.3z"/></svg></p>
-            <p v-if="false"><font-awesome-icon icon="fa-solid fa-link"/></p>
+            <p v-if="false">
+              <!--<font-awesome-icon icon="fa-solid fa-link"/>-->
+            </p>
           </button>
         </form>
       </div>
       <div class="list">
         <ul>
-          <div class="go-back" @click="flipContent(),waitToShowResult()"><font-awesome-icon icon="fa-solid fa-arrow-left" /></div>
+          <div class="go-back" @click="flipContent(),waitToShowResult()">
+          <!--<font-awesome-icon icon="fa-solid fa-arrow-left" />-->
+          </div>
           <li v-for="i in 3" :key="i" class="result">
             <span v-if="true">The URL: </span>
             <p>{{ historyLinks[Object.keys(historyLinks).length - (i) + 1].longUrl }}</p>
